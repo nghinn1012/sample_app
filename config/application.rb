318ac667aaa5ea8a78be5cd6039d454bc352a8b3
Nghinn <ngo.thao.huong@sun-asterisk.com>
2024-04-12 11:54:28 +0700

    chapter_3_4_5

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,11 @@ Bundler.require(*Rails.groups)
 module RailsTutorial
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.i18n.available_locales = [:en, :vi]
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = :vi
 
     # Configuration for the application, engines, and railties goes here.
     #
