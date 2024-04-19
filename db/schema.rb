781e358a36b68c7648e617b9c44069ab3b065af5
Nghinn <ngo.thao.huong@sun-asterisk.com>
2024-04-19 09:43:42 +0700

    chapter_7

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_16_022548) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_19_023023) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.date "date_of_birth"
+    t.string "gender"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
