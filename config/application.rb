09e4bee68b80cd003c6c5ecf52694b03b4f1394d
Nghinn <ngo.thao.huong@sun-asterisk.com>
2024-04-22 10:12:31 +0700

    chapter 13

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module RailsTutorial
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = :en
 
+    config.active_storage.variant_processor = :mini_magick
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
